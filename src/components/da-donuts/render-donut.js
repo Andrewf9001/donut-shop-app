d45b28af38734a721ba61bfe0a5e030eca787b6d
BrighamLind <brighamlind@gmail.com>
2019-06-28 15:21:36 -0600

    changed pics

--- src/components/da-donuts/render-donut.js
+++ src/components/da-donuts/render-donut.js
@@ -4,11 +4,11 @@ const RenderDonut = props => {
   console.log(props.donut.name);
   return (
     <div className="donut-item-wrapper">
-      <div>{props.donut.name}</div>
+      <div className="something">{props.donut.name}</div>
       <div>{props.donut.description}</div>
       <div>{`$ ${props.donut.price}`}</div>
       <img src={props.donut.picture} />
-      <button>updat</button>
+      <button>update</button>
     </div>
   );
 };
