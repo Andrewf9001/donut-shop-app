4e6f054431cbe44dc35ac443e203736cbc3a42b6
kysen <kysenj@gmail.com>
2019-06-28 15:27:06 -0600

    juice

--- src/components/da-donuts/render-donut.js
+++ src/components/da-donuts/render-donut.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 
 const RenderDonut = props => {
   console.log(props.donut.name);
+
   return (
-    <div className="donut-item-wrapper">
+    <div className="donut-card">
       <div>{props.donut.name}</div>
       <div>{props.donut.description}</div>
       <div>{`$ ${props.donut.price}`}</div>
