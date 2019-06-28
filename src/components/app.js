b13dad9fd17ce56d4f1a5f8f005a8c8a0f266cd9
kysen <kysenj@gmail.com>
2019-06-28 10:51:54 -0600

    kmj auth page functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import Auth from "./pages/auth";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+      <div className="app">
+        <Auth />
       </div>
     );
   }
--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,11 +1,56 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default class Auth extends Component {
-  render() {
-    return (
-      <div>
-        <h1>LOGIN SHIT</h1>
-      </div>
-    );
-  }
-}
+const Auth = () => {
+  const [userName, handleNameInput] = useState("");
+  const [userPass, handlePasswordInput] = useState("");
+  const [errorText, setErrorText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+
+  const handleLoginSubmit = e => {
+    setIsLoading(true);
+    e.preventDefault();
+    axios
+      .post("https://bottega-edonut-db.herokuapp.com/login", {
+        username: userName,
+        password: userPass
+      })
+      .then(response => {
+        setIsLoading(false);
+        console.log(response);
+        if (response.data.valid === true) {
+          setName(response.data.name);
+        } else if (response.data.valid === false) {
+          setErrorText(response.data.reason);
+        } else {
+          console.log("idek");
+        }
+      })
+      .catch(error => {
+        setErrorText("An error occurred", error);
+      });
+  };
+
+  return (
+    <div className="auth-wrapper">
+      <div>{isLoading ? "Loading" : errorText}</div>
+      <div>{name ? name : null}</div>
+      <form onSubmit={e => handleLoginSubmit(e)}>
+        <input
+          placeholder="UserName"
+          value={userName}
+          onChange={e => handleNameInput(e.target.value)}
+        />
+        <input
+          placeholder="Password"
+          value={userPass}
+          onChange={e => handlePasswordInput(e.target.value)}
+        />
+        <button type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default Auth;
